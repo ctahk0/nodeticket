fa5bf60e4a98c419f6b0706da3bbff77d9d241ba
ctahk0 <ctahk0.ct@gmail.com>
2018-08-21 13:55:44 +0200

    track odds changes

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var axios = require('axios');
 
 // mysql
  var connConf = {
+
     host     : 'localhost',
     user     : 'root',
     password : '',
@@ -27,6 +28,7 @@ var t = 35;
 setInterval(function(){ 
     // console.log(t);
     t--;
+    // getResults('2018-08-21');
     if (t == 30) {
         getOdds(6, function(teams) {
             // console.log('working ... ', ret);
@@ -44,7 +46,7 @@ setInterval(function(){
             });
         });   
     }else if (t == 0 ) {t = 35;}
-}, 1000);
+}, 1000);  // vratiti na 1000 nakon testiranja
 
 // call U/O2.5 
 // setInterval(function() {
@@ -72,11 +74,122 @@ async function readPravila(cb) {
         });
 }
 
+/** ================================================== Get results ================================================ */
+// _date in format yyyy-mm-dd
+async function getResults(_date) {
+    let teams = [];
+    try {
+        
+        conn = mysql.createConnection(connConf);
+        conn.connect();
+        console.log('Get results started!');
+        
+        let url = await axios('http://www.oddsmath.com/api/v1/events-by-day.json/?language=en&country_code=BA&timezone=Europe%2FSarajevo&day=' + _date + '&grouping_mode=0');
+        let resp = url.data.data;
+        for (let i in resp) {
+            let events = resp[i].events;
+            for (let e in events) {
+                if (events[e].hasOwnProperty('livescore')) {
+                    if (events[e].livescore.status == 'Finished'){
+                        // console.log('id - rez: ', events[e].id, ' - ', events[e].livescore.value);
+                        let _str = events[e].livescore.value;
+                        let ht = Number(_str.substring(0, _str.indexOf('-')));
+                        let at = Number(_str.substring(_str.indexOf('-') + 1));
+                        let gol = ht+at;
+                        let tip = '0'
+                        if (ht > at) {
+                            tip = '1'
+                        }else if (ht < at) {
+                            tip = '2'
+                        }else {
+                            tip = '0'
+                        }
+                        // tip = ht > at ? '1' : '2';
+                        let uo = gol > 2.5 ? 'O': 'U';
+                        teams.push([
+                            events[e].id,
+                            events[e].time,
+                            events[e].hometeam_name,
+                            events[e].awayteam_name,
+                            _str,
+                            gol,
+                            tip,
+                            uo
+                        ]);
+                        // sql1 = "UPDATE tiket SET arez = '" + _str + "', arezgolovi = " + gol + ", atip = '" + tip + "' WHERE LEFT(gameID,7) ='" + events[e].id + "'";
+                        // conn.query(sql1, function(err, result) {
+                        //     if (err) throw err;
+                        // });
+                    }
+                } 
+
+            }
+        }
+        sql = 'INSERT IGNORE INTO rezultati (gameID, dtime, hometeam, awayteam, rez, gol, tip, UO) VALUES ?';
+        conn.query(sql, [teams], function(err, result) {
+            if (err) throw err;
+            // conn.end();
+            console.log(result);
+        });
+
+        sql = 'UPDATE tiket INNER JOIN rezultati ON (LEFT(tiket.gameID,7) = rezultati.gameID) SET tiket.arez = rezultati.rez, tiket.arezgolovi = rezultati.gol, tiket.atip = rezultati.tip, tiket.auo = rezultati.uo';
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        
+        sql = "UPDATE tiket set awin = 0 WHERE atip = '0'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+
+        sql = "UPDATE tiket set awin = 1 WHERE tip = '1' AND atip = '1'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        sql = "UPDATE tiket set awin = -1 WHERE tip = '1' AND atip = '2'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+
+        sql = "UPDATE tiket set awin = 1 WHERE tip = '2' AND atip = '2'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        sql = "UPDATE tiket set awin = -1 WHERE tip = '2' AND atip = '1'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+
+        sql = "UPDATE tiket set awin = 1 WHERE tip = 'O' AND auo = 'O'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        sql = "UPDATE tiket set awin = 1 WHERE tip = 'U' AND auo = 'U'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        sql = "UPDATE tiket set awin = -1 WHERE tip = 'O' AND auo = 'U'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        sql = "UPDATE tiket set awin = -1 WHERE tip = 'U' AND auo = 'O'";
+        conn.query(sql, function(err, result) {
+            if (err) throw err;
+        });
+        console.log('results inserted successfully!');
+        conn.end();
+    }
+    catch (error) {
+        console.error(error); // 💩
+    }
+    
+}
 //============================================ get data ============================================================
 async function getOdds(market, cb){
 	// console.log(market.lo, market.hi);
     let teams = [];
     let teamsobj = [];
+    let teamsChanges = [];
     let resp;
     let o1;
     let o2;
@@ -103,7 +216,7 @@ async function getOdds(market, cb){
                         c1 = resp[i]['last']['1'];
                         c2 = resp[i]['last']['2'];
                         break;
-                    case 6:
+                    case 6: // UO2.5
                         o1 = resp[i]['first']['U'];
                         o2 = resp[i]['first']['O'];
                         c1 = resp[i]['last']['U'];
@@ -112,6 +225,15 @@ async function getOdds(market, cb){
                     default:
                         break;
                 }
+                Date.prototype.addHours= function(h){
+                    this.setHours(this.getHours()+h);
+                    return this;
+                }
+                let dat = new Date();
+                // let dat = new Date().addHours(2);
+                // console.log(dat);
+                // console.log(resp[i]['time'], ' - ', dat);
+                
                 teams.push([
                     resp[i]['x-id'],
                     resp[i]['league'],
@@ -122,6 +244,12 @@ async function getOdds(market, cb){
                     0,
                     o1,
                     o2,
+                    c1,
+                    c2
+                ]);
+                teamsChanges.push([
+                    resp[i]['x-id'],
+                    dat,
                     c1,
                     c2
                 ]);
@@ -154,26 +282,28 @@ async function getOdds(market, cb){
         conn.connect();
         console.log('market ', market);
 
-            // INSERT INTO tags (tag) VALUES ('jatin'),('test') ON DUPLICATE KEY UPDATE tag = VALUES(tag);
+        sql1 = 'DELETE FROM matches_temp WHERE 1';
+        conn.query(sql1, function(err, result) {
+            if (err) throw err;
+        });
+
         if (market == 6) {
             sql = 'INSERT IGNORE INTO matches (oID, league, country, dTime, HomeTeam, AwayTeam, played, ouo25_u, ouo25_o, cuo25_u, cuo25_o) VALUES ?';
             sql2 = 'INSERT IGNORE INTO matches_temp (oID, league, country, dTime, HomeTeam, AwayTeam, played, ouo25_u, ouo25_o, cuo25_u, cuo25_o) VALUES ?';
             sql3 = 'UPDATE matches INNER JOIN matches_temp ON (matches.oID = matches_temp.oID) SET matches.cuo25_u = matches_temp.cuo25_u, matches.cuo25_o = matches_temp.cuo25_o';
+            sql4 = 'INSERT IGNORE INTO o_changes (gameID, currtime, uo25_u, uo25_o) VALUES ?'
         } else if (market == 4) {
             sql = 'INSERT IGNORE INTO matches (oID, league, country, dTime, HomeTeam, AwayTeam, played, oah0_1, oah0_2, cah0_1, cah0_2) VALUES ?';
             sql2 = 'INSERT IGNORE INTO matches_temp (oID, league, country, dTime, HomeTeam, AwayTeam, played, oah0_1, oah0_2, cah0_1, cah0_2) VALUES ?';
             sql3 = 'UPDATE matches INNER JOIN matches_temp ON (matches.oID = matches_temp.oID) SET matches.cah0_1 = matches_temp.cah0_1, matches.cah0_2 = matches_temp.cah0_2';
+            sql4 = 'INSERT IGNORE INTO o_changes (gameID, currtime, ah0_1, ah0_2) VALUES ?'
         }
         conn.query(sql, [teams], function(err, result) {
             if (err) throw err;
             // conn.end();
             // console.log(result);
         });
-        sql1 = 'DELETE FROM matches_temp WHERE 1';
-        conn.query(sql1, function(err, result) {
-            if (err) throw err;
-        });
-        
+
         conn.query(sql2, [teams], function(err, result) {
             if (err) throw err;
         });
@@ -181,12 +311,26 @@ async function getOdds(market, cb){
         conn.query(sql3, function(err, result) {
             if (err) throw err;
         });
-        
+        // console.log(sql4);
+        // console.log(teamsChanges);
+        conn.query(sql4, [teamsChanges], function(err, result) {
+            if (err) throw err;
+            // console.log(result);
+        });
         conn.end();
         cb(teamsobj);        
     }catch (error) {
         console.error(error); // 💩
     }
+}
+/** ========================================== sleep function ============================================================ */
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
 
 //============================================ play ticket ============================================================
